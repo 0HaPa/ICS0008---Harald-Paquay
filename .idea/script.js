56e7631cc4fd270d8af7cffd548ada885f3ceba6
haral <hapaqu@taltech.ee>
2025-03-10 09:33:57 +0200

    V2

--- .idea/script.js
+++ .idea/script.js
@@ -6,3 +6,25 @@ window.onload = function () {
     hello();
     console.log("RAAAAAAAAAAAAAAAAAH!");
 }
+function ligma() {
+    let counter = 5;
+    while(counter > 0) {
+        console.log(counter);
+        counter -= 1;
+    }
+}
+
+function doWhileLoop() {
+    let counter = 5;
+    do {
+        console.log(counter);
+        counter -= 1;
+    } while(counter > 0)
+}
+
+
+
+window.onload = function () {
+    ligma();
+    doWhileLoop();
+}
